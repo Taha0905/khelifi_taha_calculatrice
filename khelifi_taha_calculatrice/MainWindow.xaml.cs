c793c371bad0be3eaa2bdb652d2db29e11f4fa66
taha khelifi <taha.khelifi@saintmichelannecy.fr>
2023-10-05 21:14:05 +0200

    2.1

--- khelifi_taha_calculatrice/MainWindow.xaml.cs
+++ khelifi_taha_calculatrice/MainWindow.xaml.cs
@@ -25,13 +25,13 @@ namespace khelifi_taha_calculatrice
             InitializeComponent();
         }
 
-        //déclaration des variables
+//déclaration des variables
         double N1 = 0;
         double N2 = 0;
         double resultat = 0;
         char operation = ' ';
 
-        //création de la fonction display
+//création de la fonction display
         public void display(string num)
         {
             if (TB_Resultat.Text == "0")
@@ -43,212 +43,12 @@ namespace khelifi_taha_calculatrice
                 TB_Resultat.Text = TB_Resultat.Text + num;
             }
         }
-        
-        //configuration des bouttons
-        private void BTN_1_Click(object sender, RoutedEventArgs e)
+
+//configuration des bouttons
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            display("1");
-        }
-
-        private void BTN_2_Click(object sender, RoutedEventArgs e)
-        {
-            display("2");
-        }
-
-        private void BTN_3_Click(object sender, RoutedEventArgs e)
-        {
-            display("3");
-        }
-
-        private void BTN_4_Click(object sender, RoutedEventArgs e)
-        {
-            display("4");
-        }
-
-        private void BTN_5_Click(object sender, RoutedEventArgs e)
-        {
-            display("5");
-        }
-
-        private void BTN_6_Click(object sender, RoutedEventArgs e)
-        {
-            display("6");
-        }
-
-        private void BTN_7_Click(object sender, RoutedEventArgs e)
-        {
-            display("7");
-        }
-
-        private void BTN_8_Click(object sender, RoutedEventArgs e)
-        {
-            display("8");
-        }
-
-        private void BTN_9_Click(object sender, RoutedEventArgs e)
-        {
-            display("9");
-        }
-
-        private void BTN_0_Click(object sender, RoutedEventArgs e)
-        {
-            display("0");
-        }
-
-        private void BTN_CLR_Click(object sender, RoutedEventArgs e)
-        {
-            TB_Resultat.Clear();
-        }
-
-        //boutton égale
-        private void BTN_Egale_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation == '+')
-            {
-                // Effectuez l'addition
-                resultat = N1 + int.Parse(TB_Resultat.Text);
-            }
-            else if (operation == '-')
-            {
-                // Effectuez la soustraction
-                resultat = N1 - int.Parse(TB_Resultat.Text);
-            }
-            else if (operation == '/')
-            {
-                int N2 = int.Parse(TB_Resultat.Text);
-
-                // Vérifiez si le dénominateur est égal à zéro
-                if (N2 == 0)
-                {
-                    TB_Resultat.Text = "Erreur : Division par zéro";
-                    return; // Sortez de la méthode pour éviter une division par zéro
-                }
-
-                // Effectuez la division
-                resultat = N1 / N2;
-            }
-            else if (operation == '*')
-            {
-                // Effectuez la multiplication
-                resultat = N1 * int.Parse(TB_Resultat.Text);
-            }
-            else if (operation == 'C')
-            {
-                // Calculez le carré du nombre
-                int nombre = int.Parse(TB_Resultat.Text);
-                resultat = nombre * nombre;
-            }
-            // Ajoutez une condition pour gérer la racine carrée
-            else if (operation == 'R')
-            {
-                // Calculez la racine carrée du nombre
-                double nombre = double.Parse(TB_Resultat.Text);
-
-                if (nombre >= 0) // Vérifiez que le nombre est positif ou nul
-                {
-                    double racineCarree = Math.Sqrt(nombre);
-                    TB_Resultat.Text = racineCarree.ToString();
-                    return; // Sortez de la méthode car la racine carrée est calculée
-                }
-                else
-                {
-                    TB_Resultat.Text = "Erreur : Nombre négatif";
-                    return; // Sortez de la méthode en cas de nombre négatif
-                }
-            }
-            // Ajoutez une condition pour gérer le pourcentage
-            else if (operation == 'P')
-            {
-                // Calculez le pourcentage
-                double nombre = double.Parse(TB_Resultat.Text);
-                double pourcentage = N1 * (nombre / 100.0);
-
-                TB_Resultat.Text = pourcentage.ToString();
-                return; // Sortez de la méthode car le pourcentage est calculé
-            }
-
-            // Affichez le résultat dans la zone de texte
-            TB_Resultat.Text = resultat.ToString();
-        }
-
-        //boutton moins
-        private void BTN_Moins_Click(object sender, RoutedEventArgs e)
-        {
-
-            N1 = int.Parse(TB_Resultat.Text);
-
-            TB_Resultat.Clear();
-
-            operation = '-';
-        }
-
-        //boutton diviser
-        private void BTN_Diviser_Click(object sender, RoutedEventArgs e)
-        {
-            N1 = int.Parse(TB_Resultat.Text);
-
-            TB_Resultat.Clear();
-
-            operation = '/';
-        }
-
-        //boutton plus
-        private void BTN_Plus_Click(object sender, RoutedEventArgs e)
-        {
-            N1 = int.Parse(TB_Resultat.Text);
-
-            TB_Resultat.Clear();
-
-            operation = '+';
-        }
-
-        //boutton fois
-        private void BTN_Fois_Click(object sender, RoutedEventArgs e)
-        {
-            N1 = int.Parse(TB_Resultat.Text);
-
-            TB_Resultat.Clear();
-
-            operation = '*';
-        }
-
-        private void BTN_carré_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(TB_Resultat.Text))
-            {
-                int nombre = int.Parse(TB_Resultat.Text);
-                int carre = nombre * nombre;
-                TB_Resultat.Text = carre.ToString();
-            }
-        }
-
-        private void BTN_racine_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(TB_Resultat.Text))
-            {
-                double nombre = double.Parse(TB_Resultat.Text);
-
-                if (nombre >= 0) // Vérifiez que le nombre est positif ou nul
-                {
-                    double racineCarree = Math.Sqrt(nombre);
-                    TB_Resultat.Text = racineCarree.ToString();
-                }
-                else
-                {
-                    TB_Resultat.Text = "Erreur : Nombre négatif";
-                }
-            }
-        }
-
-        private void BTN_pourcentage_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(TB_Resultat.Text))
-            {
-                double nombre = double.Parse(TB_Resultat.Text);
-                double pourcentage = N1 * (nombre / 100.0); // Calcule le pourcentage en multipliant le nombre par le pourcentage
-
-                TB_Resultat.Text = pourcentage.ToString();
-            }
+            Button button = (Button)sender;
+            display(button.Content.ToString()); 
         }
     }
 }
